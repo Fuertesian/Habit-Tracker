e1e3dcda20860115ee28ac2c446698c74f3e56c0
BKMohle <54755048+BKMohle@users.noreply.github.com>
2020-04-20 17:58:34 -0500

    added an array of days from 1 to 31

--- src/components/MonthlyHabitTrackerComponents/Days/Days.js
+++ src/components/MonthlyHabitTrackerComponents/Days/Days.js
@@ -1,5 +1,12 @@
 import React from 'react'
-const Days = () => {
-  return <></>
+const Days = (props) => {
+  var daysList = []
+  for (var i = 1; (i <= 31); i++) {
+    daysList.push(<p1>{i}</p1>)
+  }
+
+return daysList
+
 }
+
 export default Days
