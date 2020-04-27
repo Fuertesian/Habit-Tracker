e652452d5188d16e219b56dce94e4dd537db0daa
BKMohle <54755048+BKMohle@users.noreply.github.com>
2020-04-27 14:51:23 -0500

    Edited HabitNames and called it inside HabitTable

--- src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
+++ src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+
 const HabitNames = () => {
-  return <></>
+  return (
+    <>
+      <Form.Group>
+        <Form.Control size='sm' type='text' placeholder='Add New Habit' />
+      </Form.Group>
+    </>
+  )
 }
 export default HabitNames
