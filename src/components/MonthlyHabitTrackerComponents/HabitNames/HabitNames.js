11925eb70b7b6d13b7a09270a98f76c3d25bbe0f
BKMohle <54755048+BKMohle@users.noreply.github.com>
2020-04-27 17:11:44 -0500

    Changed form type in HabitNames

--- src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
+++ src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form'
+// import Form from 'react-bootstrap/Form'
 
 const HabitNames = () => {
   return (
-    <>
-      <Form.Group>
-        <Form.Control size='sm' type='text' placeholder='Add New Habit' />
-      </Form.Group>
-    </>
+    <form>
+      <input type='text' placeholder='Add New Habit' />
+    </form>
   )
 }
 export default HabitNames
