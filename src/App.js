38aa166a8a20264d4dfbd8005b4785b7240828be
BKMohle <54755048+BKMohle@users.noreply.github.com>
2020-04-30 14:54:45 -0500

    Added ToggleButton for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css'
 import MonthlyHabitTracker from './containers/MonthlyHabitTracker'
+import ToggleButton from './components/MonthlyHabitTrackerComponents/TableRows/ToggleButton'
 
 function App() {
-  return <MonthlyHabitTracker />
+  return (
+    <div>
+      <ToggleButton />
+      <MonthlyHabitTracker />
+    </div>
+  )
 }
 
 export default App
