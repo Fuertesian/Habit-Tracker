e27223c5a7e8d79fb307c58af7bd9e8d14a2b40a
BKMohle <54755048+BKMohle@users.noreply.github.com>
2020-05-01 13:47:18 -0500

    Added getClick and getIcon

---
+++ src/components/MonthlyHabitTrackerComponents/TableRows/ClickIndex.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+// Potentially use an onClick method within the <td></td> element itself?
+
+const ClickIndex = () => {
+  const value = ['Empty', 'Checked', 'Not Checked']
+  const [clickIndex, setClickIndex] = useState(0)
+
+  if (clickIndex > 2) {
+    setClickIndex(0)
+  }
+
+  const getClick = () => {
+    setClickIndex(clickIndex + 1)
+  }
+
+  const getIcon = () => {
+    if (clickIndex === 0) {
+      return { backgroundColor: 'white' }
+    } else if (clickIndex === 1) {
+      return { backgroundColor: 'green' }
+    } else {
+      return { backgroundColor: 'red' }
+    }
+  }
+
+  return (
+    <div>
+      <button style={getIcon()} onClick={getClick}>
+        {value[clickIndex]}
+      </button>
+      {/* <td onClick ={() => setclickIndex(clickIndex + 1)}>{value[clickIndex]}</td> */}
+    </div>
+  )
+}
+
+export default ClickIndex
