acd26b747bcdecda23244ff3adb0796d544a1299
Fuertesian <wwfuertes@gmail.com>
2020-05-12 23:31:51 -0500

    comments for mohle to review

--- src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
+++ src/components/MonthlyHabitTrackerComponents/HabitNames/HabitNames.js
@@ -2,8 +2,19 @@ import React from 'react'
 // import Form from 'react-bootstrap/Form'
 
 const HabitNames = () => {
+  // form should not be added for every new input This is what you are doing
+  /* <form>
+        <input/>
+      </form>
+      <form>
+        <input>
+      </form>
+
+      There should only be one form tag
+      Also you should be using the Input component from react-bootstrap
+      */
   return (
-    <form>
+    <form> 
       <input type='text' placeholder='Add New Habit' />
     </form>
   )
