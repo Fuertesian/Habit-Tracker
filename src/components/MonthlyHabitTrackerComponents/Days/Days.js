efa001eaa6648ba121de62e28c06e08b36715f88
Fuertesian <wwfuertes@gmail.com>
2020-05-12 23:56:45 -0500

    replaced clickIndex to be a JSON object
    getClickNumber was modified to accomodate new data object
    
    handleclick populates the state variable object
    
    getIcon modified to accomodate object state variable
    
    Added id prop to table data element

--- src/components/MonthlyHabitTrackerComponents/Days/Days.js
+++ src/components/MonthlyHabitTrackerComponents/Days/Days.js
@@ -1,35 +1,63 @@
 import React, { useState } from 'react'
+
 const Days = (props) => {
-  
   let tableDataStateList = []
   for (let num = 0; num <= 31; num++) {
     tableDataStateList.push(0)
   }
 
-  const [clickIndex, setClickIndex] = useState(0)
+  // const [clickIndex, setClickIndex] = useState(0)
+    // if (clickIndex > 2) {
+  //   setClickIndex(0)
+  // }
 
-  if (clickIndex > 2) {
-    setClickIndex(0)
+  // const getClick = () => {
+  //   setCl
+  
+  const [rowClickIndex, setRowClickIndex] = useState({})
+
+  // utility functions
+  const getClickNumber = (tableDataId, rowDataObj) => {
+    let clickNumber = rowDataObj[tableDataId]
+    if (clickNumber === undefined) {
+      clickNumber = 1
+    } else if (clickNumber === 2) {
+      clickNumber = 0
+    } else {
+      clickNumber = clickNumber + 1
+    }
+    return clickNumber
   }
 
-  const getClick = () => {
-    setClickIndex(clickIndex + 1)
+// table data functions
+  const handleClick = (event) => {
+    let obj = { ...rowClickIndex }
+    const clickNumber = getClickNumber(event.target.id, obj)
+    // debugger
+    obj[event.target.id] = clickNumber
+    setRowClickIndex(obj)
   }
 
-  const getIcon = () => {
-    if (clickIndex === 0) {
+  const getIcon = (tableDataId) => {
+    let clickNumber =
+      rowClickIndex[tableDataId] !== undefined ? rowClickIndex[tableDataId] : 0
+    if (clickNumber === 0) {
       return { backgroundColor: 'white' }
-    } else if (clickIndex === 1) {
+    } else if (clickNumber === 1) {
       return { backgroundColor: 'green' }
     } else {
-      return { backgroundColor: 'red' }
+      return { backgroundColor: 'red' } 
     }
   }
 
+  // component functions
   let daysRowList = []
   for (let i = 1; i <= 31; i++) {
-    daysRowList.push(<td key={i} style={getIcon()} onClick={getClick}></td>)
+    daysRowList.push(
+      <td id={i} key={i} style={getIcon(i)} onClick={handleClick}></td>
+    )
   }
+
 
   return daysRowList
 }
