ee99048d31e4e1d53c747f2eba1f62bdb3c56c7b
BKMohle <brandonmohle42@gmail.com>
2020-05-19 11:30:30 -0500

    Commented out HabitTable in favor of MonthTabs

--- src/containers/MonthlyHabitTracker.js
+++ src/containers/MonthlyHabitTracker.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Title from '../components/MonthlyHabitTrackerComponents/Title/Title'
 import MonthTabs from '../components/MonthlyHabitTrackerComponents/MonthTabs/MonthTabs'
 import Container from 'react-bootstrap/Container'
-import HabitTable from './HabitTable'
+// import HabitTable from './HabitTable'
 
 const MonthlyHabitTracker = (props) => {
   return (
     <Container>
       <Title />
       <MonthTabs />
-      <HabitTable />
+      {/* <HabitTable /> */}
     </Container>
   )
 }
