a7129ee6224e337b7e47e45581ad0d8b35ca4389
BKMohle <brandonmohle42@gmail.com>
2020-05-19 11:29:54 -0500

    Made tabs for three months for testing purposes

--- src/components/MonthlyHabitTrackerComponents/MonthTabs/MonthTabs.js
+++ src/components/MonthlyHabitTrackerComponents/MonthTabs/MonthTabs.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React from "react";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import HabitTable from "../../../containers/HabitTable";
 
 const MonthTabs = () => {
-  return <></>
-}
-export default MonthTabs
+  const createMonthTab = (month) => {
+    return <Tab eventKey={month} title={month}>
+      <HabitTable />
+    </Tab>;
+  };
+
+  return (
+    <>
+      <Tabs defaultActiveKey="month" id="uncontrolled-tab-example">
+        {createMonthTab("January")}
+        {createMonthTab("February")}
+        {createMonthTab("March")}
+      </Tabs>
+    </>
+  );
+};
+export default MonthTabs;
